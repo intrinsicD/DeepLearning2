cae6007f4884252a69afb1ba3357348378a5d777
intrinsicD <alexander.dieckmann@posteo.de>
2025-11-06 22:33:51 +0100

    Add HelixProp optimizer

--- src/optimizers/__init__.py
+++ src/optimizers/__init__.py
@@ -2,10 +2,12 @@
 
 from .custom_sgd import CustomSGD
 from .custom_adam import CustomAdam
+from .helixprop import HelixProp
 from .muon_fast import MuonFast
 
 __all__ = [
     'CustomSGD',
     'CustomAdam',
+    'HelixProp',
     'MuonFast',
 ]
