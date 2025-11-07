89d5b7504b43bb3217c1308b604e6eb5d0ad2e05
intrinsicD <alexander.dieckmann@posteo.de>
2025-11-07 09:54:30 +0100

    Add Vision Transformer optimizer experiment

--- src/architectures/__init__.py
+++ src/architectures/__init__.py
@@ -4,10 +4,12 @@ from .base import BaseArchitecture
 from .simple_cnn import SimpleCNN
 from .resnet import ResNet
 from .fc_net import FullyConnectedNet
+from .vision_transformer import VisionTransformer
 
 __all__ = [
     'BaseArchitecture',
     'SimpleCNN',
     'ResNet',
     'FullyConnectedNet',
+    'VisionTransformer',
 ]
--- src/experiments/__init__.py
+++ src/experiments/__init__.py
@@ -3,9 +3,11 @@
 from .mnist_example import run_experiment
 from .compare_architectures import compare_architectures
 from .compare_optimizers import compare_optimizers
+from .vit_optimizer_experiment import run_vit_optimizer_experiment
 
 __all__ = [
     'run_experiment',
     'compare_architectures',
     'compare_optimizers',
+    'run_vit_optimizer_experiment',
 ]
